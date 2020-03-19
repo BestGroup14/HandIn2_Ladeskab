21874256141897176e8d73e91d67ac6252428e7c
carolinenissen <201806156@uni.au.dk>
2020-03-19 12:00:31 +0100

    Tilføjelse af test til door

--- Unit.Test.HandIn2/Unit.Test.Door.cs
+++ Unit.Test.HandIn2/Unit.Test.Door.cs
@@ -13,7 +13,9 @@ namespace Unit.Test.HandIn2
     {
         private Door _uut;
         private Display _display;
+        private StationControl _stationControl;
 
+     
 
         [SetUp]
         public void Setup()
@@ -36,5 +38,24 @@ namespace Unit.Test.HandIn2
             _uut.UnlockDoor();
             _display.Received(1);
         }
+
+        [Test]
+        public void TestCloseDoor()
+        {
+            int numValues = 0;
+            _uut.DoorClosedEvent += (o, args) => numValues++;
+            _uut.CloseDoor();
+            Assert.That(numValues, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestopenDoor()
+        {
+            int numValues = 0;
+            _uut.DoorOpenedEvent += (o, args) => numValues++;
+            _uut.OpenDoor();
+            Assert.That(numValues, Is.EqualTo(1));
+        }
+
     }
 }
