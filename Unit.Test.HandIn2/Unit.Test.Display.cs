6b3e4dabd629b3b941b1fb020e2f80d27a45adee
Maria Louise Pedersen <marialouise.pedersen@outlook.com>
2020-03-19 10:30:15 +0100

    Test af Display

--- Unit.Test.HandIn2/Unit.Test.Display.cs
+++ Unit.Test.HandIn2/Unit.Test.Display.cs
@@ -12,40 +12,21 @@ namespace Unit.Test.HandIn2
     [TestFixture]
     public class TestDisplay
     {
-        private StationControl _uut;
-        private IDisplay _display;
-        private IDoor _door;
-        private IRFIDReader _rfidReader;
-        private IUsbCharger _usbCharger;
-        private ILogFile _logFile;
+        private Display _uut;
+
 
         [SetUp]
         public void Setup()
         {
-            _display = Substitute.For<IDisplay>();
-            _door = Substitute.For<IDoor>();
-            _rfidReader = Substitute.For<IRFIDReader>();
-            _usbCharger = Substitute.For<IUsbCharger>();
-            _logFile = Substitute.For<ILogFile>();
-
-            _uut = new StationControl(_door,_rfidReader,_display,_usbCharger,_logFile);
+            _uut = Substitute.For<Display>();
         }
 
 
-        //[Test]
-        //public void TestMethod1()
-        //{
-        //    string testString = "Ladeskab låst";
-        //    _door.LockDoor();
-
-        //    Assert.That(_display.ShowMessage(testString));
-
-            
-        //    _display.ShowMessage(testString);
-
-        //    Assert.That(testString, Is.EqualTo("Tilslut telefon"));
-
-        //}
+        [Test]
+        public void string_check_Tilsluttelefon()
+        {
+            _uut.Received(1).ShowMessage("Tilslut telefon");
+        }
 
 
     }
